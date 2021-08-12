eeb9dfdf79a36a77411678c9a9c56e1946a13a13
Gabriel Allba <rielgasy9@gmail.com>
2021-08-12 20:16:46 +0700

    Change responsive navbar style

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-let navbutton = document.getElementById('nav-mobile')
-let navmenu =  document.getElementById('mobile-navigation')
-let closebutton = document.getElementById('close-nav')
+let togglelist = document.getElementById('toggle')
+let navmenu =  document.getElementById('navigations')
 
-navmenu.hidden = true
-
-navbutton.addEventListener('click',function(){
-    navmenu.hidden = false;
-    navbutton.style.display = 'none'
-    closebutton.hidden = false
-    
-    closebutton.style.display = 'flex'
-    closebutton.style.justifyContent = 'flex-end'
-    closebutton.style.width = '100%'
-    closebutton.style.cursor = 'pointer'
-})
-
-closebutton.addEventListener('click', function(){
-    navbutton.style.display = 'flex'
-    navbutton.style.justifyContent = 'flex-end'
-    closebutton.style.display = 'none'
-    navmenu.hidden = true
+togglelist.addEventListener('click', function(){
+    navmenu.classList.toggle('active')
 })
